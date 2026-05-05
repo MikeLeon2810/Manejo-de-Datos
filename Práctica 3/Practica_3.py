d4a19aae75d86e8fb706205d986361cf4c19a832
Miguel_León <miguelleon@ciencias.unam.mx>
2026-05-04 21:47:49 -0600

    Update problemas 4 y 5, falta revisar que se cumpla todo bien

--- Práctica 3/Practica_3.py
+++ Práctica 3/Practica_3.py
@@ -103,6 +103,167 @@ def valida_ean(codigo):
     return es_valido, pais
 
 
+# ---------------------------
+# PROBLEMA 4: Lista Ligada Circular + Josephus
+# ---------------------------
+# ---------------------------
+# PROBLEMA 4: Lista Circular + Josephus
+# ---------------------------
+
+class Lista_circular:
+
+    class Nodo:
+        def __init__(self, dato):
+            self.dato = dato
+            self.next = None
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insertar(self, dato):
+        nuevo = self.Nodo(dato)
+
+        if self.head is None:
+            self.head = nuevo
+            self.tail = nuevo
+            nuevo.next = self.head
+        else:
+            self.tail.next = nuevo
+            self.tail = nuevo
+            self.tail.next = self.head
+
+    def __str__(self):
+        if self.head is None:
+            return ""
+
+        cad = ""
+        aux = self.head
+
+        while True:
+            cad += str(aux.dato) + " -> "
+            aux = aux.next
+            if aux == self.head:
+                break
+
+        return cad
+
+
+def josephus(n, k):
+    lista = Lista_circular()
+
+    # Crear círculo de soldados
+    for i in range(1, n+1):
+        lista.insertar(i)
+
+    actual = lista.head
+    prev = lista.tail
+
+    eliminados = []
+
+    while actual.next != actual:
+        # avanzar k-1 pasos
+        for _ in range(k-1):
+            prev = actual
+            actual = actual.next
+
+        eliminados.append(actual.dato)
+
+        # eliminar nodo
+        prev.next = actual.next
+        actual = actual.next
+
+    sobreviviente = actual.dato
+
+    return eliminados, sobreviviente
+
+
+# ---------------------------
+# PROBLEMA 5: Torres de Hanoi con PILAS
+# ---------------------------
+
+class Pila:
+    
+    def __init__(self):
+        self.pila = []
+        self.tope = -1
+
+    def push(self, elemento):
+        self.pila.append(elemento)
+        self.tope += 1
+
+    def pop(self):
+        if self.tope >= 0:
+            ultimo = self.pila[self.tope]
+            self.pila.pop()
+            self.tope -= 1
+            return ultimo
+        else:
+            return None
+
+    def peek(self):
+        if self.tope >= 0:
+            return self.pila[self.tope]
+        return None
+
+
+def mover_disco(origen, destino, nombre_origen, nombre_destino, movimientos):
+    top_origen = origen.pop()
+    top_destino = destino.pop()
+
+    if top_origen is None:
+        origen.push(top_destino)
+        movimientos.append(f"Mover disco de {nombre_destino} -> {nombre_origen}")
+    
+    elif top_destino is None:
+        destino.push(top_origen)
+        movimientos.append(f"Mover disco de {nombre_origen} -> {nombre_destino}")
+    
+    elif top_origen > top_destino:
+        origen.push(top_origen)
+        origen.push(top_destino)
+        movimientos.append(f"Mover disco de {nombre_destino} -> {nombre_origen}")
+    
+    else:
+        destino.push(top_destino)
+        destino.push(top_origen)
+        movimientos.append(f"Mover disco de {nombre_origen} -> {nombre_destino}")
+
+
+def torres_Hanoi(n):
+    A = Pila()
+    B = Pila()
+    C = Pila()
+
+    movimientos = []
+
+    # llenar torre A
+    for i in range(n, 0, -1):
+        A.push(i)
+
+    total_movimientos = 2**n - 1
+
+    # si n es par intercambiar destino y auxiliar
+    if n % 2 == 0:
+        B, C = C, B
+        nombre_B, nombre_C = 'C', 'B'
+    else:
+        nombre_B, nombre_C = 'B', 'C'
+
+    nombre_A = 'A'
+
+    for i in range(1, total_movimientos + 1):
+        if i % 3 == 1:
+            mover_disco(A, C, nombre_A, nombre_C, movimientos)
+        elif i % 3 == 2:
+            mover_disco(A, B, nombre_A, nombre_B, movimientos)
+        else:
+            mover_disco(B, C, nombre_B, nombre_C, movimientos)
+
+    return movimientos
+
+
+
 
 # ---------------------------
 # MAIN
@@ -134,7 +295,27 @@ También puedes llamar a Maria al 555-123-4567, o al (555) 987-6543."""
         else:
             print(f"{c} -> Válido: {valido}")
 
+ # Problema 4
+    print("\n----------- Problema 4 -----------")
 
+    n = 25
+    k = 2
+
+    eliminados, sobreviviente = josephus(n, k)
+
+    print("Orden de eliminación:", eliminados)
+    print("Soldado que sobrevive:", sobreviviente)
+#Problema 5
+    print("\n----------- Problema 5 -----------")
+
+    n = 3 #Numero de discos
+    movimientos = torres_Hanoi(n)
+
+    for m in movimientos:
+        print(m)
+
+    print("Total de movimientos:", len(movimientos))
+        
 # ---------------------------
 # ENTRY POINT
 # ---------------------------
